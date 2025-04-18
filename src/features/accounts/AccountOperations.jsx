f55e9393268a03878cf957d3bb5218ae48614e24
Au Cao Thuy Duong <auduong.301@gmail.com>
2025-04-18 17:20:17 +0700

    update

--- src/features/accounts/AccountOperations.jsx
+++ src/features/accounts/AccountOperations.jsx
@@ -21,15 +21,8 @@ function AccountOperations() {
       <div className="inputs">
         <div>
           <label>Deposit</label>
-          <input
-            type="number"
-            value={depositAmount}
-            onChange={(e) => setDepositAmount(+e.target.value)}
-          />
-          <select
-            value={currency}
-            onChange={(e) => setCurrency(e.target.value)}
-          >
+          <input type="number" value={depositAmount} onChange={(e) => setDepositAmount(+e.target.value)} />
+          <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
             <option value="USD">US Dollar</option>
             <option value="EUR">Euro</option>
             <option value="GBP">British Pound</option>
@@ -40,29 +33,14 @@ function AccountOperations() {
 
         <div>
           <label>Withdraw</label>
-          <input
-            type="number"
-            value={withdrawalAmount}
-            onChange={(e) => setWithdrawalAmount(+e.target.value)}
-          />
-          <button onClick={handleWithdrawal}>
-            Withdraw {withdrawalAmount}
-          </button>
+          <input type="number" value={withdrawalAmount} onChange={(e) => setWithdrawalAmount(+e.target.value)} />
+          <button onClick={handleWithdrawal}>Withdraw {withdrawalAmount}</button>
         </div>
 
         <div>
           <label>Request loan</label>
-          <input
-            type="number"
-            value={loanAmount}
-            onChange={(e) => setLoanAmount(+e.target.value)}
-            placeholder="Loan amount"
-          />
-          <input
-            value={loanPurpose}
-            onChange={(e) => setLoanPurpose(e.target.value)}
-            placeholder="Loan purpose"
-          />
+          <input type="number" value={loanAmount} onChange={(e) => setLoanAmount(+e.target.value)} placeholder="Loan amount" />
+          <input value={loanPurpose} onChange={(e) => setLoanPurpose(e.target.value)} placeholder="Loan purpose" />
           <button onClick={handleRequestLoan}>Request loan</button>
         </div>
 
